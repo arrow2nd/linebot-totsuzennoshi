c34666986ae0295d7a83c667254d39d8926859e7
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-20 15:01:20 +0900

    style: スタイルを調整

--- api/index.js
+++ api/index.js
@@ -15,38 +15,39 @@ const client = new line.Client(config)
 const app = express()
 app.get('/', (_req, res) => res.send('ok! (GET)'))
 app.post('/hook/', line.middleware(config), async (req, res) => {
-  await Promise.all(req.body.events.map(e => main(e)))
+  await Promise.all(req.body.events.map((e) => main(e)))
   res.status(200).end()
 })
 
-/**
- * 突然の死
- *
- * @param {Object} ev イベントオブジェクト
- */
-async function main (ev) {
+// メイン
+async function main(ev) {
   // テキスト以外の場合
   if (ev.message.type !== 'text') {
     await client.replyMessage(ev.replyToken, {
       type: 'text',
-      text: '＿人人人人人人人人人人人人人＿\n＞　テキストでお願いします　＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣'
+      text:
+        '＿人人人人人人人人人人人人人＿\n＞　テキストでお願いします　＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣'
     })
     return
   }
 
   // AA作成
   const lines = ev.message.text.split('\n')
-  const lenMax = Math.max(...lines.map(v => getLength(v)))
+  const lenMax = Math.max(...lines.map((v) => getLength(v)))
   const toge = ['＿人' + '人'.repeat(lenMax) + '人＿']
 
   // 真ん中
-  lines.forEach(text => toge.push('＞　' + text + '　'.repeat(lenMax - getLength(text)) + '　＜'))
+  lines.forEach((text) =>
+    toge.push('＞　' + text + '　'.repeat(lenMax - getLength(text)) + '　＜')
+  )
 
   // 下
   toge.push('￣Y^' + 'Y^'.repeat(lenMax) + 'Y￣')
 
   // 返信
-  const result = toge.reduce((accumulator, currentValue) => accumulator + '\n' + currentValue)
+  const result = toge.reduce(
+    (accumulator, currentValue) => accumulator + '\n' + currentValue
+  )
   await client.replyMessage(ev.replyToken, {
     type: 'text',
     text: result
@@ -59,10 +60,10 @@ async function main (ev) {
  * @param  {String} text テキスト
  * @return {Number}      全角での文字列の長さ
  */
-function getLength (text) {
+function getLength(text) {
   const lines = split(text)
   let len = 0
-  lines.forEach(value => {
+  lines.forEach((value) => {
     // eslint-disable-next-line no-control-regex
     if (!value.match(/[^\x01-\x7E]/) || !value.match(/[^\uFF65-\uFF9F]/)) {
       len += 0.5
@@ -74,4 +75,6 @@ function getLength (text) {
 }
 
 // vercel
-(process.env.NOW_REGION) ? module.exports = app : app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+process.env.NOW_REGION
+  ? (module.exports = app)
+  : app.listen(PORT, () => console.log(`Listening on ${PORT}`))
